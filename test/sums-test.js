d9a6c0e8784b41bc5fc33f08d12e861bf3e0249a
Glenn Jones <glennjonesnet@gmail.com>
2013-11-10 12:44:26 +0000

    Correct js lint issues

--- test/sums-test.js
+++ test/sums-test.js
@@ -1,9 +1,9 @@
 'use strict';
 
 var hapi        = require('hapi'),
-    assert      = require("assert"),
+    assert      = require('assert'),
     chai        = require('chai'),
-    assert  	= chai.assert,
+    assert      = chai.assert,
     routes      = require('../lib/routes.js');
 
 
@@ -21,29 +21,29 @@ describe('add endpoint', function(){
     server.inject({method: 'GET', url: '/sum/add/5/5'}, function (res) {
         assert.deepEqual(
         {
-            "equals": 10      
+            'equals': 10      
         }, JSON.parse(res.payload));
         done();
     });
-  })
+  });
 
 
   it('add - should error if a string is passed', function(done){
     server.inject({method: 'GET', url: '/sum/add/100/x'}, function (res) {
         assert.deepEqual(
         {
-          "code": 400,
-          "error": "Bad Request",
-          "message": "the value of b must be a number",
-          "validation": {
-            "source": "path",
-            "keys": [
-              "b"
+          'code': 400,
+          'error': 'Bad Request',
+          'message': 'the value of b must be a number',
+          'validation': {
+            'source': 'path',
+            'keys': [
+              'b'
             ]
           }
         }, JSON.parse(res.payload));
         done();
     });
-  })
+  });
 
-})
+});
