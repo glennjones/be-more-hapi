0bdacf8aad8d0d95d4a6456136988d21f8d254ba
Glenn Jones <glennjonesnet@gmail.com>
2015-08-28 07:56:14 +0100

    Muiltple server connection example

--- bin/with-connections.js
+++ bin/with-connections.js
@@ -1,122 +1,56 @@
-/*
+
+// Used to test server connections and plugins documentation
 
 'use strict';
-var Hapi            = require('hapi'),
-    Swagger         = require('hapi-swagger'),
-    Blipp           = require('blipp'),
-    Pack            = require('../package'),
-    Routes          = require('../lib/routes.js');
-    
-
-
-//Example of the use of more than one connection
-
+var Hapi        = require('hapi'),
+    Inert       = require('inert'),
+    Vision      = require('vision'),
+    Blipp       = require('blipp'),
+    Hoek        = require('hoek'),
+    Pack        = require('../package');
 
 var server = new Hapi.Server();
-server.connection({ 
-    host: 'localhost', 
-    port: 3001, 
-    labels: 'a' 
-});
-server.connection({ 
-    host: 'localhost',
-    port: 3002, 
-    labels: 'b' 
-});
+
+server.connection({ host: 'localhost', port: 3000, labels: 'a' });
+server.connection({ host: 'localhost', port: 3001, labels: 'b' });
+server.connection({ host: 'localhost', port: 3002, labels: 'c' });
 
 
 
-// plug-in with routes
-var apiPlugin = {
-    register: function (plugin, options, next) {
-
-        plugin.route({
-            method: 'GET',
-            path: '/api',
-            config: {
-                handler: function (request, reply) {
-                    reply('Main DELETE');
-                },
-                description: 'Main GET',
-                tags: ['api']
-                
-            }
-        });
-
-        return next();
-    }
+var plugin1 = function (server, options, next) {
+    server.route({
+        method: 'GET',
+        path: '/plugin1',
+        config: {
+            handler: function (request, reply) {
+                reply('Hi from plugin 1');
+            },
+            description: 'plugin1',
+            tags: ['api']
+        }
+    });
+    next();
 };
+plugin1.attributes = { name: 'plugin1' };
 
-apiPlugin.register.attributes = {
-    name: 'an api plugin',
-    version: '0.0.1'
+
+
+var plugin2 = function (server, options, next) {
+    server.route({
+        method: 'GET',
+        path: '/plugin2',
+        config: {
+            handler: function (request, reply) {
+                reply('Hi from plugin 2');
+            },
+            description: 'plugin2',
+            tags: ['api']
+        }
+    });
+    next();
 };
+plugin2.attributes = { name: 'plugin2' };
 
-
-// plug-in with routes
-var mainPlugin = {
-    register: function (plugin, options, next) {
-
-        plugin.route({
-            method: 'GET',
-            path: '/main',
-            config: {
-                handler: function (request, reply) {
-                    reply('Main GET');
-                },
-                description: 'Main post',
-                tags: ['api']
-            }
-        });
-
-        plugin.route({
-            method: 'POST',
-            path: '/main/{foo}/comment/{bar}',
-            config: {
-                handler: function (request, reply) {
-                    reply('Main post');
-                },
-                description: 'Main POST',
-                tags: ['api']
-            }
-        });
-
-        plugin.route({
-            method: 'DELETE',
-            path: '/main/{id}',
-            config: {
-                handler: function (request, reply) {
-                    reply('Main DELETE');
-                },
-                description: 'Main post',
-                tags: ['api']
-            }
-        });
-
-
-        return next();
-    }
-};
-
-mainPlugin.register.attributes = {
-    name: 'main',
-    version: '0.1.1'
-};
-
-
-
-// add all the normal routes to both connectsion
-server.route(Routes.routes);
-
-
-// setup template engine
-server.views({
-        path: 'templates',
-        engines: { html: require('handlebars') },
-        partialsPath: './templates/withPartials',
-        helpersPath: './templates/helpers',
-        isCached: false
-    })
 
 
 // setup swagger options
@@ -138,32 +72,51 @@ var swaggerOptions = {
     }
 };
 
+var swaggerOptionsA = Hoek.clone(swaggerOptions),
+    swaggerOptionsB = Hoek.clone(swaggerOptions)
+
+swaggerOptionsA.basePath = 'http://localhost:3000';
+swaggerOptionsB.basePath = 'http://localhost:3001';
 
 
 
-
-
-// register all the plug-ins and start server
-server.register([{
-        register: require('hapi-swagger'), 
-        options: swaggerOptions
-    },{
-        register: require('blipp')
-    }], function (err) {
-            server.register([mainPlugin], { select: 'a' }, function (err) {
-                server.register([apiPlugin], { select: 'b', routes: {prefix: '/v2'} }, function (err) {
-                   
-                    console.log(['hapi-swagger','start'], 'swagger interface loaded')
-                    server.start(function(){
-                        console.log(['hapi','start'], Pack.name + ' - web interface: ' + server.info.uri);
+// defaults
+server.register([
+    Blipp,
+    Inert,
+    Vision,
+], function (err) {
+    if (err) {
+        throw err;
+    }
+    
+    // interface a
+    server.register([
+        plugin1,
+        {
+            register: require('hapi-swagger'),
+            options: swaggerOptionsA
+        }], { select: ['a'] }, function (err) {
+            if (err) {
+                throw err;
+            }
+            
+            // interface b
+            server.register([
+                plugin2,
+                {
+                    register: require('hapi-swagger'),
+                    options: swaggerOptionsB
+                }], { select: ['b'] }, function (err) {
+                    if (err) {
+                        throw err;
+                    }
+                    server.start(function () {
+                        console.log('Server running at:', server.info.uri);
                     });
-
                 });
-            });
+
         });
 
-
-
-*/
-
+});
 
